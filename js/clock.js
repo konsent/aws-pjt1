3698d26184e9835272f20a1f41601020f7a30ee6
konsent <konsent2@gmail.com>
2022-01-22 17:36:01 +0900

    get Time and show it to HTML

---
+++ js/clock.js
@@ -0,0 +1,10 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+    const date = new Date();
+    const currentTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    clock.innerText = currentTime;
+};
+
+getClock();
+setInterval(getClock,1000);
