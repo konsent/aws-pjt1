8f94e4e98c5a96f951690f181b86e97f993ee7fa
konsent <konsent2@gmail.com>
2022-01-22 16:29:37 +0900

    Re-adjusting unnecessary code

--- app.js
+++ app.js
@@ -9,16 +9,15 @@ const USER_NAME = "username";
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USER_NAME, username);
-    paintGreeting(username);
+    localStorage.setItem(USER_NAME, loginInput.value);
+    paintGreeting();
 }
 
-function paintGreeting(name){
-    greeting.innerText = `Hello ${name}`;
+function paintGreeting(){
+    const username = localStorage.getItem(USER_NAME);
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
-
 
 const savedUserName = localStorage.getItem(USER_NAME);
 
@@ -27,5 +26,5 @@ if(savedUserName === null){
     loginForm.addEventListener("submit", onLoginSubmit);
 
 } else if(savedUserName != null) {
-    paintGreeting(savedUserName);
+    paintGreeting();
 };
