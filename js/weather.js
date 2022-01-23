b35c67359ec3e568d70e6309762fb5c54dccb234
konsent <konsent2@gmail.com>
2022-01-23 09:11:18 +0900

    Setting weather API and bring location and weather on HTML

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,20 @@
+const API_KEY = "b0b607eaf41301cf84cd733d7407ea3c"
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log(`You live in ${lat} ${lng}`);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            city.innerText = data.name;
+
+    })
 };
+
 function onGeoError(){
     console.log("Can't find your location");
 };
